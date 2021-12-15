dc62977b0dbde2a02debd16572a338e69ac790f1
ikmiyabi <miyabi1452@yahoo.co.jp>
2021-12-16 03:12:48 +0900

    add: circleci設定を追加

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git add . &&\
-git stash &&\
-yarn build &&\
-git checkout deploy &&\
-rm -rf docs &&\
-mv dist/ docs/ &&\
-git add . &&\
-git commit -m deploy &&\
-git push origin deploy &&\
-git checkout main &&\
-git stash pop &&\
-open https://ikmiyabi.github.io/knowledge
+echo "deploy start"
+
+yarn build
+git checkout deploy
+rm -rf docs
+mv dist/ docs/
+git add .
+git commit -m deploy
+git push origin deploy
+git checkout main
+
+echo "deploy end"
