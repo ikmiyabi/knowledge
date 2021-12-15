87870dd104246b4961471af422adfc188ec5ee4e
ikmiyabi <miyabi1452@yahoo.co.jp>
2021-12-16 03:35:05 +0900

    fix: CIスクリプト更新

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,12 @@
 echo "deploy start"
 
 yarn build &&\
-
-mv .gitignore.deploy .gitignore
-git checkout deploy
-rm -rf docs
-mv dist/ docs/
-git add -A
-git commit -m "[ci skip] deploy"
+git checkout deploy &&\
+mv .gitignore.deploy .gitignore &&\
+rm -rf docs &&\
+mv dist/ docs/ &&\
+git add -A &&\
+git commit -m "[ci skip] deploy" &&\
 git push -f origin deploy &&\
 
 echo "deploy end"
