f1109afbf0ee3f86a478d4d1f957040f7b27ab9b
ikmiyabi <miyabi1452@yahoo.co.jp>
2021-12-19 13:35:43 +0000

    [ci skip] deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,10 +9,10 @@ module.exports = {
   dest: 'dist/',
   public: 'docs/',
   head: [
-    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicon_16x16.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon_32x32.png' }],
-    ['link', { rel: 'apple-touch-icon', href: '/images/apple-touch-icon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/knowledge/images/favicon_16x16.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/knowledge/images/favicon_32x32.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/knowledge/images/apple-touch-icon.png' }],
+    ['link', { rel: 'manifest', href: '/knowledge/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#AFA2EB' }],
   ],
   plugins: [
