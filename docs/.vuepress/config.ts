abe082f9db3636af7e346f57304bbd12f1790386
ikmiyabi <miyabi1452@yahoo.co.jp>
2024-06-08 08:12:44 +0000

    [ci skip] deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,18 +1,19 @@
 import { getSidebarContents, getNavbarContents } from './itemConfig'
 
 module.exports = {
-  base: '/knowledge/',
+  base: '/',
   lang: 'ja',
   title: 'Knowledge',
   description: '個人用開発ナレッジ',
   serviceWorker: true,
   dest: 'dist/',
   public: 'docs/',
+  toggleSidebar: true,
   head: [
-    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/knowledge/images/favicon_16x16.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/knowledge/images/favicon_32x32.png' }],
-    ['link', { rel: 'apple-touch-icon', href: '/knowledge/images/apple-touch-icon.png' }],
-    ['link', { rel: 'manifest', href: '/knowledge/manifest.webmanifest' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicon_16x16.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon_32x32.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/apple-touch-icon.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'application-name', content: 'Knowledge' }],
     ['meta', { name: 'apple-mobile-web-app-title', content: 'Knowledge' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
